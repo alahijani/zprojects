df78ccdafb27406942c86d4088f15f22b06305db
Ali Lahijani <alahijani@gmail.com>
2012-12-13 21:39:48 +0330

    test save() followed by findByUsername()

--- src/test/java/org/github/alahijani/zprojects/service/UserServiceTest.java
+++ src/test/java/org/github/alahijani/zprojects/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package org.github.alahijani.zprojects.service;
 
+import junit.framework.Assert;
 import org.github.alahijani.zprojects.model.User;
 import org.junit.Test;
 
@@ -10,6 +11,8 @@ import javax.persistence.PersistenceException;
  * @author Ali Lahijani
  */
 public class UserServiceTest extends BaseServiceTest {
+    @Resource
+    private UserService service;
 
     @Test(expected = PersistenceException.class)
     public void testNullUsername() throws Exception {
@@ -22,20 +25,22 @@ public class UserServiceTest extends BaseServiceTest {
     @Test(expected = PersistenceException.class)
     public void testDuplicateUsername() throws Exception {
         User user1 = new User();
-        user1.setUsername("jimmy");
+        user1.setUsername("jimmy-123-1");
         service.save(user1);
 
         User user2 = new User();
-        user2.setUsername("jimmy");
+        user2.setUsername("jimmy-123-1");
         service.save(user2);
 
         em.flush();
     }
 
-    @Resource
-    private UserService service;
+    @Test
     public void testSaveAndFindByUsername() throws Exception {
         User user = new User();
+        user.setUsername("jimmy-123-1");
         service.save(user);
+
+        Assert.assertEquals(service.findByUsername("jimmy-123-1").getUsername(), "jimmy-123-1");
     }
 }
