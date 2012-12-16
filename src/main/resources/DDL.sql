f07ddf04a9e0de940c41b8c4a0f4c1abfbf9c056
Ali Lahijani <alahijani@gmail.com>
2012-12-16 22:10:10 +0330

    updated schema

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -1,34 +1,74 @@
-CREATE TABLE SEQUENCE
-(
-    SEQ_NAME varchar(50) PRIMARY KEY NOT NULL,
-    SEQ_COUNT decimal(15)
-)
-CREATE TABLE PROJECTS
-(
-    ID varchar(255) PRIMARY KEY NOT NULL,
-    CODE varchar(255),
-    DESCRIPTION varchar(255),
-    TITLE varchar(255)
-)
-CREATE TABLE TASKS
-(
-    ID varchar(255) PRIMARY KEY NOT NULL,
-    DESCRIPTION varchar(255),
-    TITLE varchar(255),
-    PROJECT_ID varchar(255),
-    FOREIGN KEY ( PROJECT_ID ) REFERENCES PROJECTS ( ID )
-)
-CREATE TABLE USERS
-(
-    ID varchar(255) PRIMARY KEY NOT NULL,
-    FULLNAME varchar(255),
-    USERNAME varchar(255) NOT NULL,
-    PASSWORD varchar(255) NOT NULL,
-    ADMIN smallint DEFAULT 0,
-    ENABLED smallint DEFAULT 0
-)
+CREATE TABLE users 
+  ( 
+     id       VARCHAR(255) NOT NULL, 
+     admin    SMALLINT DEFAULT 0, 
+     enabled  SMALLINT DEFAULT 0, 
+     fullname VARCHAR(255), 
+     password VARCHAR(255) NOT NULL, 
+     username VARCHAR(255) NOT NULL UNIQUE, 
+     PRIMARY KEY (id) 
+  ) 
 
-INSERT INTO USERS(ID, FULLNAME, USERNAME, PASSWORD, ADMIN, ENABLED) VALUES (0, 'Administrator', 'admin', 'admin', 1, 1);
-INSERT INTO PROJECTS(ID, TITLE, CODE, DESCRIPTION) VALUES (0, 'Urban Velociraptor Training Project', 'uvelo',
-  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-);
+CREATE TABLE projects 
+  ( 
+     id          VARCHAR(255) NOT NULL, 
+     code        VARCHAR(255) NOT NULL UNIQUE, 
+     description VARCHAR(255) NOT NULL, 
+     title       VARCHAR(255) NOT NULL, 
+     PRIMARY KEY (id) 
+  ) 
+
+CREATE TABLE tasks 
+  ( 
+     id          VARCHAR(255) NOT NULL, 
+     description VARCHAR(255) NOT NULL, 
+     title       VARCHAR(255) NOT NULL, 
+     assignee_id VARCHAR(255), 
+     project_id  VARCHAR(255) NOT NULL, 
+     PRIMARY KEY (id) 
+  ) 
+
+ALTER TABLE tasks 
+  ADD CONSTRAINT tasks_project_id FOREIGN KEY (project_id) REFERENCES projects ( 
+  id) 
+
+ALTER TABLE tasks 
+  ADD CONSTRAINT tasks_assignee_id FOREIGN KEY (assignee_id) REFERENCES users ( 
+  id) 
+
+CREATE TABLE sequence 
+  ( 
+     seq_name  VARCHAR(50) NOT NULL, 
+     seq_count DECIMAL(15), 
+     PRIMARY KEY (seq_name) 
+  ) 
+
+INSERT INTO sequence 
+            (seq_name, 
+             seq_count) 
+VALUES      ('SEQ_GEN', 
+             10)
+
+INSERT INTO users 
+            (id, 
+             fullname, 
+             username, 
+             password, 
+             admin, 
+             enabled) 
+VALUES      (0, 
+             'Administrator', 
+             'admin', 
+             'admin', 
+             1, 
+             1)
+
+INSERT INTO projects 
+            (id, 
+             title, 
+             code, 
+             description) 
+VALUES      (1, 
+             'Urban Velociraptor Training Project', 
+             'uvelo', 
+             'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
